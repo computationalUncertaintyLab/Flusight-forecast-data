07513955a2c4f97761b98512fb57f7f4a9f001bd
tom mcandrew <mcandrew@lehigh.edu>
2022-10-12 23:41:47 -0400

    update cluster script

--- data-forecasts/LUcompUncertLab-HWAR2/SLURM.sh
+++ data-forecasts/LUcompUncertLab-HWAR2/SLURM.sh
@@ -17,8 +17,8 @@ cd ${SLURM_SUBMIT_DIR} # cd to directory where you submitted the job
  
 #--launch job
 module load anaconda3
-module load conda/biostats
-#export PYTHONPATH=$PYTHONPATH:$HOME/pythonpkgs
+#module load conda/biostats
+export PYTHONPATH=$PYTHONPATH:$HOME/pypkgs
 
 #--export environmental variables
 export LOCATION=${LOCATION}
