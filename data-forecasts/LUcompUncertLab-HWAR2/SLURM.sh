b9ab18c534d59356d1a4b97bab03f469a49fbfb4
tom mcandrew <mcandrew@lehigh.edu>
2022-10-12 23:31:39 -0400

    adding cluster scripts

---
+++ data-forecasts/LUcompUncertLab-HWAR2/SLURM.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#SBATCH --partition=health,lts,hawkcpu,infolab,engi,eng
+ 
+#--Request 1 hour of computing time
+#SBATCH --time=01:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=3
+ 
+#--Give a name to your job to aid in monitoring
+#SBATCH --job-name flumodel
+ 
+#--Write Standard Output and Error
+#SBATCH --output="myjob.%j.%N.out"
+ 
+cd ${SLURM_SUBMIT_DIR} # cd to directory where you submitted the job
+ 
+#--launch job
+module load anaconda3
+module load conda/biostats
+#export PYTHONPATH=$PYTHONPATH:$HOME/pythonpkgs
+
+#--export environmental variables
+export LOCATION=${LOCATION}
+make python3 holt_winters.py --LOCATION ${LOCATION}
+ 
+exit
