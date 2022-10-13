c36dff643e9a34c48a5948e04a03271d6dff1eca
tom mcandrew <mcandrew@lehigh.edu>
2022-10-12 22:41:03 -0400

    starting dev on baseline model for team

---
+++ data-forecasts/LUcompUncertLab-HWAR2/holt_winters.py
@@ -0,0 +1,229 @@
+#mcandrew
+
+import sys
+from collections import namedtuple
+
+import numpy as np
+import pandas as pd
+
+import stan
+
+import statsmodels.api as sm
+
+#--find next monday
+def next_monday(dat=0):
+    from datetime import datetime,timedelta
+    dt  = datetime.now()
+    day = dt.weekday()
+
+    counter = 0
+    while day%7 !=0:
+        counter+=1
+        day+=1
+
+    if dat:
+        return (dt+timedelta(days=counter)).strftime("%Y-%m-%d")
+    return counter
+
+def next_saturday_after_monday_submission( num_days_until_monday ):
+    from datetime import datetime, timedelta
+    dt  = datetime.now()+ timedelta(days=num_days_until_monday)
+
+    while dt.weekday() !=5 :
+        dt = dt + timedelta(days=1)
+    return dt.strftime("%Y-%m-%d")
+
+def collect_target_end_dates(first_saturday):
+    from datetime import datetime, timedelta
+    sat = datetime.strptime(first_saturday,'%Y-%m-%d')
+
+    target_end_dates = [first_saturday]
+    for _ in range(3):
+        sat =sat + timedelta(days=7)
+        target_end_dates.append( sat.strftime("%Y-%m-%d") )
+    return target_end_dates 
+
+def define_submission_date(num_days_until_monday):
+    from datetime import datetime, timedelta
+    now  = datetime.now()
+    monday_submission = now+timedelta(days=num_days_until_monday)
+
+    return monday_submission.strftime("%Y-%m-%d")
+
+if __name__ == "__main__":
+
+    flu = pd.read_csv("../../2022FluSightNew/data-truth/truth-Incident Hospitalizations.csv")
+    flu["date"] = pd.to_datetime(flu.date)
+    
+    flu2022 = flu.loc[flu.date > "2021-10-01"]
+
+    LOCATION = "US"
+    US = flu2022.loc[flu2022.location==LOCATION]
+
+    cut_US = US.iloc[:18]
+
+    #detrend
+    ys = cut_US.value.values
+    xs = np.arange(0,len(cut_US))
+
+    T = len(ys)
+
+    def fit_HW(ys):
+        from statsmodels.tsa.holtwinters import ExponentialSmoothing
+        model = ExponentialSmoothing( ys , trend = 'add')
+        model = model.fit()
+
+        y_infer, y_forecast  = model.fittedvalues,model.forecast(4)
+        return y_infer, y_forecast
+    y_infer, y_forecast = fit_HW(ys)
+    
+    resids = ys - y_infer
+    
+    #standardize 
+    params = namedtuple("params","avg sd")
+    stand__params   = params( avg = resids.mean(), sd = resids.std() )
+    centered_resids = (resids - stand__params.avg)/stand__params.sd
+    
+    stan_model = '''
+        data {
+           int T;
+           vector [T] y;
+        }
+        parameters { 
+           real beta0; 
+           real <lower=-1,upper = 1>beta [2]; 
+           real <lower = 0> sigma;
+        }
+        model {
+
+              beta0~normal(0,10);
+              for (l in 1:2){
+                 beta[l]~normal(0,10);
+              }
+              sigma~cauchy(5,5);
+
+             for (t in 3:T){
+                 //form predictor
+                 real mu = 0;
+                 for (l in 1:2){
+                    mu+= beta[l]*y[t-l];
+                 }
+                target+=normal_lpdf(y[t]|beta0+mu,sigma);
+             }
+        }
+        generated quantities {
+           vector [T] y_hat; 
+           vector [2+4] y_forecast;
+
+           //inference
+           for (l in 1:2){
+              y_hat[l] = y[l];
+           } 
+           for (t in 3:T){
+             //form predictor
+             real mu = 0;
+             for (l in 1:2){
+                mu+= beta[l]*y[t-l];
+             }
+             y_hat[t] = normal_rng(beta0+mu,sigma);
+           }
+
+          //Forecasting
+
+          for (l in 1:2){
+             y_forecast[l] = y[T+1-l];
+          }
+          for (t in 2+1:2+4){
+             //form predictor
+             real mu = 0;
+             for (l in 1:2){
+                mu+= beta[l]*y_forecast[t-l];
+             }
+             y_forecast[t] = normal_rng(beta0+mu,sigma);
+          }
+        }  
+    '''
+    model_data = {"T":len(centered_resids), "y":centered_resids}
+    posterior = stan.build(stan_model, data=model_data)
+    fit = posterior.sample(num_chains=4, num_samples=1000)
+    
+    #uncentered_second_diffs = (fit.get("y_hat")*second_diffs__params.sd) + second_diffs__params.avg
+    uncentered_resids = fit.get("y_hat")*stand__params.sd + stand__params.avg
+    
+    orig = {"n":[], "pred":[], "t":[]}
+    for n,sample in enumerate(uncentered_resids.T):
+        T = len(sample)
+        times = np.arange(0,T)
+        orig["pred"].extend( sample + y_infer )
+
+        T = len(sample)
+        orig["t"].extend(times)
+        orig["n"].extend( [n]*T )
+    inferences = pd.DataFrame(orig)
+
+    reference_time = len(ys)
+    uncentered_resid_forecasts = fit.get("y_forecast")*stand__params.sd + stand__params.avg
+    orig = {"n":[], "pred":[], "t":[]}
+    for n,sample in enumerate(uncentered_resid_forecasts.T):
+
+        T = 4
+        times = np.arange(reference_time,reference_time+T)
+        orig["pred"].extend( sample[-4:] + y_forecast  )
+
+        orig["t"].extend(times)
+        orig["n"].extend( [n]*T )
+    predictions = pd.DataFrame(orig)
+
+    predictions.loc[predictions["pred"]<0,"pred"] = 0
+
+    wide = pd.pivot_table(index = "n", columns = "t", values = "pred", data = predictions)
+
+    Q = np.round([0.01,0.025] + list(np.arange(0.05,0.95+0.05,0.05)) + [0.975,0.99],2)
+    N = len(Q)
+    
+    quantiles = np.percentile(wide,100*Q,axis=0)
+
+    number_of_days_until_monday = next_monday()
+    monday = next_monday(True)
+    
+    next_sat = next_saturday_after_monday_submission( number_of_days_until_monday )
+    target_end_dates = collect_target_end_dates(next_sat)
+    
+    forecast = {"target":[], "target_end_date":[], "quantile":[], "value":[]}
+    for n,week_ahead_prediction in enumerate(quantiles.T):
+        forecast["value"].extend(week_ahead_prediction)
+        forecast["quantile"].extend(Q)
+
+        #--items that need to be repeated Q times
+        forecast["target"].extend( ["{:d} wk ahead inc flu hosp".format(n+1)]*N)
+        forecast["target_end_date"].extend( [target_end_dates[n]]*N)
+        
+    forecast = pd.DataFrame(forecast)
+    forecast["location"]      = LOCATION
+    forecast["type"]          = "quantile"
+    forecast["forecast_date"] = monday
+    
+    
+    # times = np.arange(reference_time,reference_time+4)
+    
+    # fig,ax = plt.subplots()
+
+    # ax.plot(US.value.values, color = "black", alpha=0.5)
+    # ax.plot(cut_US.value.values,color="black")
+
+    # #ax.plot(yhat, color="red")
+    
+    # sns.lineplot(x="t",y="pred",data = inferences,ax=ax)
+    
+    #wide = pd.pivot_table(index = "n", columns = "t", values = "pred", data = predictions)
+    #quantiles = np.percentile(wide,[2.5,10,50,90,97.5],axis=0)
+
+    # ax.fill_between(times, quantiles[0], quantiles[-1], color="blue",alpha=0.1 )
+    # ax.fill_between(times, quantiles[1], quantiles[-2], color="blue",alpha=0.1 )
+    # ax.plot(times, quantiles[2], color="blue" )
+
+    # ax.set_xlim(0,40)
+
+    # plt.show()
+
+    
