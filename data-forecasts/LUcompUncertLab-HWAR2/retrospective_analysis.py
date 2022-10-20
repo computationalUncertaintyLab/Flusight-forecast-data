eac945f9304e63c12771093e02acc9c05e528f09
tom mcandrew <mcandrew@lehigh.edu>
2022-10-20 07:52:10 -0400

    add retro py files

---
+++ data-forecasts/LUcompUncertLab-HWAR2/retrospective_analysis.py
@@ -0,0 +1,41 @@
+#mcandrew
+
+import sys
+import numpy as np
+import pandas as pd
+
+if __name__ == "__main__":
+
+    training_dates = pd.read_csv("./retrospective_analysis/training_dates.csv")
+
+    for training_date in training_dates:
+        
+        
+        
+
+    
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
