64b97997f7067fb0bc2487f113206c2745c60311
tom mcandrew <mcandrew@lehigh.edu>
2023-01-23 23:00:41 -0500

    update

--- data-forecasts/LUcompUncertLab-hier_mech_model/hier_mech_model__2.0.2.py
+++ data-forecasts/LUcompUncertLab-hier_mech_model/hier_mech_model__2.0.2.py
@@ -550,8 +550,8 @@ if __name__ == "__main__":
 
     score_crossval = lambda P,Q: score_over_params(P,Q,model_data) 
     
-    combos = [x for x in itertools.product(np.linspace(0.001,2.0,24),[100.], [100.])]
-    results = Parallel(n_jobs=30)(delayed(score_crossval)(p,[q,r]) for (p,q,r) in combos)
+    combos = [x for x in itertools.product(np.linspace(0.001,2.5,24*4),[100.], [100.])]
+    results = Parallel(n_jobs=24)(delayed(score_crossval)(p,[q,r]) for (p,q,r) in combos)
     results = sorted(results)
 
     best_beta_param, best_phis = results[0][-2:]
